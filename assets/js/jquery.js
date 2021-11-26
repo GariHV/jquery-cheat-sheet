b5857ac43bce353027548ff35e5cd7c993499336
Garikoitz Herrero Vargas <gariherrero@gmail.com>
2021-11-26 19:36:29 +0100

    update of js and jq since event 7

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,5 +1,43 @@
 /* EVENT 1 */
 
 var btnTestEvent1Jq = $('.test-jq-event1').on('click', function () {
-    $(document).ready($('.result-jq-event1').text('is loaded!'))
+    $(document).ready($('.result-jq-event1').text('Is loaded!'))
+})
+
+/* EVENT 2 */
+
+var btnTestEvent2Jq = $('.test-jq-event2').on('click', function () {
+    $('.result-jq-event2').text('Is clicked!')
+})
+
+/* EVENT 3 */
+
+var btnTestEvent3Jq = $('.test-jq-event3').dblclick(function () {
+    $('.result-jq-event3').text('Is double clicked!')
+})
+
+/* EVENT 4 */
+
+window.addEventListener('keydown', function (e) {
+    document.querySelector('.result-jq-event4').innerHTML = `You pressed: ${e.key}`;
+}, false);
+
+
+/* EVENT 5 */
+
+$(document).bind('mousemove', function (e) {
+    $(".result-jq-event5").text("Mouse location X: " + e.pageX + ", Mouse location Y: " + e.pageY);
+})
+
+/* EVENT 6 */
+
+$('.test-jq-event6').on('input', function (e) {
+    $(".result-jq-event6").text('Changed!')
+});
+
+/* EVENT 7 */
+var img7jq = $('.test-jq-event7').on('click', function () {
+    $('.test-jq-event7').load('events.html', function () {
+        $('.result-jq-event7').text('Image is loaded!');
+    })
 })
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,50 @@
 var btnTestEvent1 = document.querySelector('.test-event1');
 
 btnTestEvent1.addEventListener('click', function () {
-    document.onload(document.querySelector('.result-event1').innerText = 'is loaded!')
+    document.onload(document.querySelector('.result-event1').innerText = 'Is loaded!')
 })
 
 /* EVENT 2 */
+
+var btnTestEvent2 = document.querySelector('.test-event2');
+
+btnTestEvent2.addEventListener('click', function () {
+    document.querySelector('.result-event2').innerText = 'Is clicked!'
+})
+
+/* EVENT 3 */
+
+var btnTestEvent3 = document.querySelector('.test-event3');
+
+btnTestEvent3.addEventListener('dblclick', function () {
+    document.querySelector('.result-event3').innerText = 'Is double clicked!'
+})
+
+/* EVENT 4 */
+
+var keyboardJsTest = document.querySelector('.keyboard-js-test')
+
+window.addEventListener('keydown', function (e) {
+    document.querySelector('.result-event4').innerHTML = `You pressed: ${e.key}`;
+}, false);
+
+/* EVENT 5 */
+
+function mouseMoveEvent5(e) {
+    document.querySelector('.result-event5').innerHTML = (`mouse location = X: ${e.x}, Y: ${e.y}`)
+}
+
+/* EVENT 6 */
+
+var inputEvent6 = document.querySelector(".test-event6").addEventListener("change", function () {
+    document.querySelector('.result-event6').innerText = 'Changed!';
+})
+
+/* EVENT 7 */
+
+var img7 = document.querySelector('.test-event7');
+img7.addEventListener('click', function () {
+    if (img7.src) {
+        document.querySelector('.result-event7').innerText = 'Image is loaded!';
+    }
+})
